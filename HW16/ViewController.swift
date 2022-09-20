3a20fb87c47774945ee1504e7628d9eaefdf43a1
BorisKiselev <hugh.dancy@icloud.com>
2022-09-20 19:48:16 +0700

    Initial Commit

---
+++ HW16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW16
+//
+//  Created by Борис Киселев on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
